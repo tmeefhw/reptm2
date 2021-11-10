dc560b81d6b5c203a7de99a521127c6405dc237e
tmeefhw <94054482+tmeefhw@users.noreply.github.com>
2021-11-10 15:09:56 +0100

    Add files via upload

---
+++ subtask1.py
@@ -0,0 +1,19 @@
+    # 7.3 Numsquare
+    # Function to find numbers
+def square_func(z):
+    #print("Enter number:")
+   
+#helper variables
+    x = 0
+    y = 1
+
+    while (x**2 > z) or (z >= y**2):
+        
+        x = x+1
+        y = y+1
+    
+        p = (x**2)
+    
+    print(p)   
+    
+square_func(17)
---
+++ subtask2.py
@@ -0,0 +1,26 @@
+
+def arit_func():
+    #start values
+        n = 0
+        s = 0
+        m = 0
+        a = 0
+        #set of values for x
+        num = [5,3,6,30,76,-1]
+
+        x = num[n]
+        m = x
+        while x != -1:
+            s = s + x
+            if m > x:
+                m = x
+            n = n + 1   #select next value
+            x = num [n]
+
+        if n == 0:
+            a = -1
+            m = -1
+        else:
+            a = s/n     #mean
+        print (n,s,m,a) #output
+arit_func()             #call function 
